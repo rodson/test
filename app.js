749a6442bf33e736a50b55c614c14850c592a8f0
rodson <rodsonluo@gmail.com>
2016-01-04 21:54:55 +0800

    add build files"

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-for (var i = 0; i < 10; i++) {
-   if (i < 7) {
-       console.log('hello');
-  } else {
-       console.log('world');
-  }
-}
+var http = require('http');
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 'hello this is test file'
 var a = 33;
 'please test me'
+
+if (a < 1) {
+}
